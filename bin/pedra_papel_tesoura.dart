8e92de60a25c1290349c082736875fae06b21cf9
Rodrigo Bossini <professorbossini@gmail.com>
2023-08-19 11:15:22 -0300

    ap02(dart): modelo pedra papel tesoura

---
+++ bin/pedra_papel_tesoura.dart
@@ -0,0 +1,29 @@
+//import para captura de dados
+
+//import para geração de valores aleatórios
+
+//1: pedra, 2: papel, 3: tesoura, 4: sair usando um enum
+void main(){
+  //exibir menu
+
+  //capturar a opção do usuário, fazendo validação com
+  //do//while
+
+  //se o usuário digitar 4, sair
+
+  //senão
+
+  //sortear a opção do computador
+
+  //mapear a opção do usuário de int para enum
+
+  //mapear a opção do computador de int para enum
+
+  //exibir as opções de cada um
+  //você (pedra) vs (tesoura) computador
+
+  //verificar o resultado (vencedor ou empate) e exibir
+
+  //exibir *********************************8
+  //esperar 5 segundos
+}
